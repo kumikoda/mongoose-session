0b231da5161e2d9eeb3bbb0a5f5f1f30eb2a649c
Anson Chu <anson.ch@gmai.com>
2013-04-08 16:42:55 -0700

    refactored code into auth library

---
+++ authentication.js
@@ -0,0 +1,107 @@
+/*
+
+  This library connects mongodb, mongoose, connect-mongo, passport and express
+  #todo: clean this up so it is more library-like
+*/
+
+
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy
+  , mongodb = require('mongodb')
+  , mongoose = require('mongoose')
+  , bcrypt = require('bcrypt')
+  , SALT_WORK_FACTOR = 10
+  , db = mongoose.connection;
+
+
+var authentication = exports = module.exports = {
+    passport:passport,
+    mongoose:mongoose,
+    ensureAuthenticated:ensureAuthenticated,
+}
+
+// Mongo and Mongoose
+mongoose.connect('localhost', 'test');
+
+// Database config 
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+  console.log('Connected to DB');
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
+
+// passsport config
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  User.findOne({ username: username }, function(err, user) {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) return done(err);
+      if(isMatch) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Invalid password' });
+      }
+    });
+  });
+}));
+
+// User Schema
+var userSchema = mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true},
+});
+
+// Bcrypt middleware
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  if(!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if(err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if(err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Password verification
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+// Seed a user
+var User = mongoose.model('User', userSchema);
+var user = new User({ username: 'anson', email: 'anson@example.com', password: '123' });
+user.save(function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('user: ' + user.username + " saved.");
+  }
+});
+
+
